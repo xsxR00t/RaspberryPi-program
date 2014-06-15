4959c1395f469a39c93b84442711657ea7b41409
cannabis <tanaka_G_taro@yahoo.co.jp>
2014-06-15 15:41:19 +0900

    arduino connect

---
+++ i2c_connect_to_arduino_class.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: Katsuya Yamaguchi
+
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+class I2cConnects:
+	def __init__(self, address):
+		# Target address
+		self.address = address
+
+	def writeData(self, data):
+		bus.write_byte( self.address, data )
+
+	def readData(self):
+		rcv = bus.read_byte( self.address )
+		return rcv
+
+def main():
+	cnt1 = I2cConnects(0x0a)
+	cnt2 = I2cConnects(0x0c)
+
+	while True:
+		var = input("Enter number1 : ")
+		if not var:
+			continue
+
+		var2 = input("Enter number2 : ")
+		if not var2:
+			continue
+
+		# Send data
+		cnt1.writeData(var)
+		cnt2.writeData(var2)
+		print "RPI -> arduino(%d) : %d" % (cnt1.address, var)
+		print "RPI -> arduino(%d) : %d" % (cnt2.address, var2)
+		time.sleep(1)
+
+		# Read data
+		print "Arduino(%d) -> RPI : %d" % (cnt1.address, cnt1.readData())
+		print "Arduino(%d) -> RPI : %d" % (cnt2.address, cnt2.readData())
+		print
+
+if __name__ == '__main__':
+	main()
