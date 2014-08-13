71f4182479a56a8b0b38f84624c3da091e408806
Hirokazu Yokoyama <hirokazu.online@gmail.com>
2014-08-14 02:54:29 +0900

    I2CConnetを複数バイトの同時送受信に対応させ，テスト

--- main-system/module/i2c_class.py
+++ main-system/module/i2c_class.py
@@ -1,22 +1,59 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# @author: Katsuya Yamaguchi
+# @author: Katsuya Yamaguchi, Hirokazu Yokoyama
 
 import smbus
 
 class I2CConnect :
-	bus = smbus.SMBus(1)
+    bus = smbus.SMBus(1)
 
-	def __init__( self, address ) :
-		# Target address
-		self.address = address
+    ''' @param address 通信先のスレーブアドレス '''
+    def __init__( self, address ) :
+        # Target address
+        self.address = address
+        self.isValidate()
 
-	def writeDate( self, data ) :
-		self.bus.write_byte( self.address, data )
-		print "Write!"
+    ''' スレーブにデータを送信します．
+    @param *data 送信するデータ
+    '''
+    def writeDate( self, *data ) :
+        self.bus.write_i2c_block_data( self.address, 0x00, list(data[0:]) )
+        print "Write!"
 
-	def readDate( self ) :
-		return self.bus.read_byte( self.address )
+    ''' スレーブからデータを受信します．(スレーブに送信リクエストを送ります．)
+    @param byteCount 受信するデータ量(bytes), default=1
+    '''
+    def readDate( self, byteCount = 1 ) :
+        return self.bus.read_i2c_block_data(self.address, 0x00, byteCount)
 
-	def test( self ) :
-		print "get"
+    def isValidate(self):
+        if self.address <= 0 or self.address > 255:
+            raise IndexError("I2C address must be an integer from 1 to 255. The specified number %s is invalid" % self.address)
+
+    def test( self ) :
+        print "get"
+
+
+# テスト
+if __name__ == '__main__':
+    '''
+        指定したアドレスのスレーブにランダムに生成した値を3つ送信する．
+        続いて，スレーブから3バイトのデータを読み出す．
+    '''
+
+    # スレーブと送受信するデータ量(byte)
+    SEND_DATA_LENGTH = 4
+
+    print "Lets start the test of I2CConnect!\n"
+    address = input("Enter number of slave address 1 - 255 > ")
+    if not address:
+        print "You must enter integer from 1 to 255."
+
+    import random, time
+    i2c = I2CConnect(address)
+    sendDataList = [random.randint(1, 255) for i in range(SEND_DATA_LENGTH)]
+    print "Send to %d, %s" % (i2c.address, sendDataList)
+    i2c.writeDate(*sendDataList)
+    time.sleep(0.1)
+    readDataList = i2c.readDate(4)
+    print "Read data from slave %d, %s" % (i2c.address, readDataList)
