e66ca3b5e4294b6dbf47e6b447eb7009066d5ff1
Hirokazu Yokoyama <hirokazu.online@gmail.com>
2014-08-18 17:19:43 +0900

    update for motor moving

--- main-system/module/i2c_class.py
+++ main-system/module/i2c_class.py
@@ -11,20 +11,22 @@ class I2CConnect :
     def __init__( self, address ) :
         # Target address
         self.address = address
-        self.isValidate()
+        self.is_validate()
 
     ''' スレーブにデータを送信します．
     @param *data 送信するデータ
     '''
     def write_data( self, *data ) :
-        self.bus.write_i2c_block_data( self.address, 0x00, list(data[0:]) )
-        print "Write!"
+        self.bus.write_i2c_block_data( self.address, data[0], list(data[0:]) )
+        #self.bus.write_byte(self.address, data[0])
+        print "Write! %s" % str(data)
 
     ''' スレーブからデータを受信します．(スレーブに送信リクエストを送ります．)
     @param byteCount 受信するデータ量(bytes), default=1
     '''
     def read_data( self, byteCount = 1 ) :
-        return self.bus.read_i2c_block_data(self.address, 0x00, byteCount)
+        #return self.bus.read_i2c_block_data(self.address, 0x00, byteCount)
+        return  self.bus.read_byte(self.address)
 
     def is_validate(self):
         if self.address <= 0 or self.address > 255:
@@ -42,18 +44,21 @@ if __name__ == '__main__':
     '''
 
     # スレーブと送受信するデータ量(byte)
-    SEND_DATA_LENGTH = 4
+    SEND_DATA_LENGTH = 3
 
     print "Lets start the test of I2CConnect!\n"
-    address = input("Enter number of slave address 1 - 255 > ")
+    #address = input("Enter number of slave address 1 - 255 > ")
+    address = 0x10
     if not address:
         print "You must enter integer from 1 to 255."
 
     import random, time
     i2c = I2CConnect(address)
-    send_data_list = [random.randint(1, 255) for i in range(SEND_DATA_LENGTH)]
+    #send_data_list = [random.randint(1, 255) for i in range(SEND_DATA_LENGTH)]
+    #send_data_list = [0b11111110, 0b11111110, 0b11111110]
+    send_data_list = [0b11111111, 0b11111111, 0b11111111]
     print "Send to %d, %s" % (i2c.address, send_data_list)
-    i2c.write_date(*send_data_list)
+    i2c.write_data(*send_data_list)
     time.sleep(0.1)
-    read_data_list = i2c.read_data(4)
+    read_data_list = i2c.read_data(SEND_DATA_LENGTH)
     print "Read data from slave %d, %s" % (i2c.address, read_data_list)
